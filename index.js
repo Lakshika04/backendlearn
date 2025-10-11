b4c3691972c4c7d787b136e00f66cbceb17dbac0
Lakshika04 <krishikaa204@gmail.com>
2025-10-12 04:17:07 +0530

    hash, jwt

--- index.js
+++ index.js
@@ -1,59 +1,69 @@
+
 import express from 'express'
 import mongoose from 'mongoose'
-import User from './models/User.js';
-const app= express();
+import User from './models/User.js'
+import bcrypt from 'bcrypt'
+import jwt from "jsonwebtoken"
+const app = express();
 
 app.use(express.json())
 
-
-
-const connectDb = async ()=>{
+const connectDb = async()=>{
     try{
         await mongoose.connect("mongodb+srv://lakshikabourai_db_user:OwgMQtE90zsJH5TK@lakshika-cluster.qy6jmlv.mongodb.net/?retryWrites=true&w=majority&appName=Lakshika-Cluster")
-        console.log("🚀 database connect successfully")
+        console.log("database connected succesfully")
     }
     catch(error){
-        console.error("❌ connection failed",error)
+        console.log("connection failed",error)
     }
 }
+
 connectDb();
 
-app.post("/signup", async (req,res)=>{
-    try {
-        const {name , email ,password} = req.body;
-        console.log("Data Received:", name, email, password);
+app.post("/signup",async(req,res)=>{
+    try{
+        const{name,email,password}=req.body;
+        console.log("Data Received:", name,email,password);
         const existingUser = await User.findOne({email});
         if(existingUser){
-            return res.status(400).json({message:"User already exists"})
+            return res.status(400).json({message:"user already exist"})
         }
 
-        // Create a new user 
-        const newuser = new User({name,email,password});
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password,salt);
+
+        //create newuser
+        const newuser = new User({name,email,password:hashedPassword});
         await newuser.save();
-
-        res.status(201).json({message:"User created Successfully"})
-    } catch (error) {
-        console.error("Error during signup:",error)
-        res.status(500).json({message:"Internal Server Error"})
+        res.status(201).json({message:"User created successfully"})
+    }
+    catch(error){
+        console.error("error during signup:",error)
+        res.status(500).json({message:"Internal server error"})
     }
 })
-
-app.post("/login",async (req, res)=>{
+app.post("/login",async(req,res)=>{
     try{
-        const {email, password}= req.body;
-        const user = await User.findOne({email, password});
+        const {email,password}=req.body;
+        const user = await User.findOne({email});
         if(!user){
-            return res.status(500).json({message:"User nhi mila"});
+            return res.status(200).json({message:"User not found"});
         }
-        res.status(200).json({message:"Login Successfully",user})
-    }catch(error){
+        
+        const isPasswordValid = await bcrypt.compare(password,user.password);
+        if(!isPasswordValid){
+            return res.status(400).json({message:"Invalid credentials"})
+        }
+        
+        const token = jwt.sign({id:user._id,email:user.email},"Khy0POz9KV5uk3zImnL/Pe2YVWaTkvJuLzWJ5Va08P2zyd2sXPin461DoTgTLpZG",{expiresIn:"1h"})
 
-        console.error("Error during Login:", error)
-        res.status(500).json({message:"Internal Server Error"})
+        res.status(200).json({message:"Login Successfully",user,token})
+    }
+    catch(error){
+        console.error("error during login:", error)
+        res.status(500).json({message:"Internal server error"})
     }
 })
-
-
-app.listen(5000,()=>{
+ app.listen(5000,()=>{
     console.log("server is running on 5000 port");
-})
+ })
--- models/User.js
+++ models/User.js
@@ -1,22 +1,22 @@
-import mongoose from "mongoose";
 
+import mongoose from "mongoose";
 const userSchema = new mongoose.Schema({
     name:{
-        type:String,
-        required:true,
-        trim:true
+        type: String,
+        required: true,
+        trim: true
     },
     email:{
-        type:String,
-        required:true,
-        unique:true,
+        type: String,
+        required: true,
+        unique: true,
     },
     password:{
-        type:String,
-        required:true,
+        type: String,
+        required: true,
     }
-},{timestamps:true})
+    
+}, {timestamps: true})
 
-const User = mongoose.model("User",userSchema);
-
+const User = mongoose.model("User", userSchema);
 export default User;
